ddd8cd70237412b1ea16634c53d79aa2e7518982
Osmay Y. Cruz Alvarez <osmay.cruz@gmail.com>
2015-09-14 12:19:39 +0300

    Fixed update token method

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ console.log('\033[31m Session object: \033[0m ');
 console.log(session);
 console.log('\n');
 
-console.log('\033[31m Generate token string:\033[0m ')
+console.log('\033[31m Generate token string:\033[0m ');
 var token = tokenize.create(session);
 //token += 'inject_some_nasty_code'
 console.log(token);
@@ -25,5 +25,14 @@ console.log('\n');
 
 console.log('\033[31m Recovered session info from token \033[0m ');
 var rsession = tokenize.validate(token);
+console.log(rsession);
+console.log('\n');
+
+console.log('Updating the current token');
+var newtoken = tokenize.update(token);
+console.log(newtoken);
+
+console.log('\033[31m Recovered session info from updated token \033[0m ');
+rsession = tokenize.validate(newtoken);
 console.log(rsession);
 console.log('\n');
